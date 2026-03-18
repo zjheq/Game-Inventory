edad772bfabe5e01a31da601b98da13df3297087
zj <zanjoesoliveres26@gmail.com>
2026-03-18 20:18:11 +0800

    inventory system
    add a menu

--- main.py
+++ main.py
@@ -7,10 +7,50 @@ def display(stuff):
     
     print("Inventory: ")
     total = 0
-    for i in stuff:
-        print(f"{i}: {stuff[i]}")
-        total += stuff[i] 
+    for k, v in stuff.items():
+        print(f"{v} {k}")
+        total += v 
         
     print(f"total items: {total}")
+
+def addItem(inv, loot):
     
-display(inventory)
+    for i in loot: 
+        if i in inv:
+            inv[i]+=1
+        
+        else:
+            inv[i] = 1
+
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+    
+    
+while True:
+    print("""
+\nDecide your action:
+
+1: Show inventory
+2: Get dragon Loot
+3: Exit                         
+          """
+          )
+    try:
+        user = int(input("> ").strip())
+    
+    except ValueError:
+          print("Numbers only!")
+          continue
+    
+    if user == 1:  
+        display(inventory)
+    
+    elif user == 2:
+        addItem(inventory, dragonLoot)
+    
+    elif user == 3:
+        break
+    
+    else:
+        print("1-3 only!")
